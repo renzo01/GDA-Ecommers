255f0d960f3dda10bdafc0d9a87c8e3ae35e8bbd
renzo01 <renzogomez577@gmail.com>
2020-08-27 15:14:05 -0500

    Carrousel

--- src/components/Body/MainPage/MainPage.component.jsx
+++ src/components/Body/MainPage/MainPage.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import CarrouselOfferProducts from "../../Commons/Carrosel-Offer-Products/CarrouselOfferProducts.component";
-
+import "./MainPage.styles.css";
 const MainPage = () => {
   return (
-    <div>
+    <div className='main-page'>
+      <h4 className='offers-title'>OFFERTAS</h4>
       <CarrouselOfferProducts />
     </div>
   );
--- src/components/Commons/Carrosel-Offer-Products/CarrouselOfferProducts.component.jsx
+++ src/components/Commons/Carrosel-Offer-Products/CarrouselOfferProducts.component.jsx
@@ -1,10 +1,37 @@
 import React from "react";
 import OfferProduct from "../Offer-Product/OfferProduct.component";
 import Prod1 from "../../../assets/product/producto1.jpg";
+import Carrousel from "react-elastic-carousel";
+import "./CarrouselOfferProducts.styles.css";
 const CarrouselOfferProducts = () => {
   return (
-    <div>
-      <OfferProduct image={Prod1} />
+    <div className='carrousel-offer-products'>
+      <Carrousel itemsToShow={4}>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+        <item>
+          <OfferProduct image={Prod1} />
+        </item>
+      </Carrousel>
     </div>
   );
 };
